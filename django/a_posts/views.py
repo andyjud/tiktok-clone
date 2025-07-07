6e18cc75eb828981025d64a97cd10cec9899b1bd
Andreas Jud <andreas.jud@gmail.com>
2025-07-07 17:25:57 +0200

    Add files via upload

--- django/a_posts/views.py
+++ django/a_posts/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from .models import Post
 
 def home_view(request):
-    return render(request, 'a_posts/home.html')
+    posts = Post.objects.all()
+    context = {
+        'page': 'Home',
+        'posts': posts,
+    }
+    if request.htmx:
+        return render(request, 'a_posts/partials/_home.html', context)
+    return render(request, 'a_posts/home.html', context)
+
+
+def explore_view(request):
+    context = {
+        'page': 'Explore',
+    }
+    if request.htmx:
+        return render(request, 'a_posts/partials/_explore.html', context)
+    return render(request, 'a_posts/explore.html', context)
+
+
+def upload_view(request):
+    context = {
+        'page': 'Upload',
+    }
+    if request.htmx:
+        return render(request, 'a_posts/partials/_upload.html', context)
+    return render(request, 'a_posts/upload.html', context)
