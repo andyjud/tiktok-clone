ea895cf6f1f459a9d4b0c622454cea98b4e88351
Andreas Jud <andreas.jud@gmail.com>
2025-09-03 09:00:32 +0200

    Update views.py

--- django/a_users/views.py
+++ django/a_users/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
 User = get_user_model()
 
+
+def index_view(request):
+    return render(request, 'a_users/index.html')
+
+
+@login_required
 def profile_view(request, username):
     profile_user = get_object_or_404(User, username=username)
     
