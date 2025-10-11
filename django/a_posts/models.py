ae6ff877ce31c87144401ebf0783d8b49aeb222c
Andreas Jud <andreas.jud@gmail.com>
2025-10-11 18:37:59 +0200

    Update models.py

--- django/a_posts/models.py
+++ django/a_posts/models.py
@@ -8,7 +8,21 @@ class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
     body = models.CharField(max_length=80, null=True, blank=True)
     tags = models.CharField(max_length=80, null=True, blank=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likedposts", through="LikedPost")
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at'] 
     
     def __str__(self):
         return str(self.uuid) 
+    
+    
+class LikedPost(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at'] 
+        unique_together = ('user', 'post')
