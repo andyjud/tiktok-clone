0865e60fd58ac3d681c47c860f612502a88e7692
Andreas Jud <andreas.jud@gmail.com>
2025-10-16 08:59:40 +0200

    Update admin.py

--- django/a_posts/admin.py
+++ django/a_posts/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, LikedPost, BookmarkedPost
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'author', 'body')
+    
+    
+@admin.register(LikedPost)
+class LikedPostAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'user', 'post')
+    
+    
+@admin.register(BookmarkedPost)
+class BookmarkedPostAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'user', 'post')
