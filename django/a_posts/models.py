df7b5f2e1ca672623e39ac5aed574051c13100c2
Andreas Jud <andreas.jud@gmail.com>
2025-10-16 08:59:24 +0200

    Add bookmarks functionality to Post model

--- django/a_posts/models.py
+++ django/a_posts/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     body = models.CharField(max_length=80, null=True, blank=True)
     tags = models.CharField(max_length=80, null=True, blank=True)
     likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likedposts", through="LikedPost")
+    bookmarks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="bookmarkedposts", through="BookmarkedPost")
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
@@ -19,6 +20,16 @@ class Post(models.Model):
     
     
 class LikedPost(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at'] 
+        unique_together = ('user', 'post')
+        
+        
+class BookmarkedPost(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
