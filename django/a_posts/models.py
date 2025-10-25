50e5a69b0131e2841d0f2c288e7461e67ac80789
Andreas Jud <andreas.jud@gmail.com>
2025-10-25 11:47:54 +0200

    Add parent_comments property to Post model

--- django/a_posts/models.py
+++ django/a_posts/models.py
@@ -12,6 +12,10 @@ class Post(models.Model):
     bookmarks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="bookmarkedposts", through="BookmarkedPost")
     created_at = models.DateTimeField(auto_now_add=True)
     
+    @property
+    def parent_comments(self):
+        return self.comments.filter(parent_comment__isnull=True)
+    
     class Meta:
         ordering = ['-created_at'] 
     
@@ -37,3 +41,30 @@ class BookmarkedPost(models.Model):
     class Meta:
         ordering = ['-created_at'] 
         unique_together = ('user', 'post')
+        
+        
+        
+class Comment(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)
+    post = models.ForeignKey('Post', related_name='comments', on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
+    body = models.CharField(max_length=250)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likedcomments", through="LikedComment")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Comment by {self.author} | {self.created_at.strftime('%b %d, %Y')} | {self.uuid}" 
+        
+
+class LikedComment(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at'] 
+        unique_together = ('user', 'comment')
