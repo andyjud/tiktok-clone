0b3adea9771951d988a07ecb80bc3d6bf1110c04
Andreas Jud <andreas.jud@gmail.com>
2025-10-30 16:47:15 +0100

    Add additional URL patterns for post actions

--- django/a_posts/urls.py
+++ django/a_posts/urls.py
@@ -4,4 +4,10 @@ from .views import *
 urlpatterns = [
     path('', post_page_view),
     path('<pk>/', post_page_view, name='post_page'),
+    path('post_edit/<pk>/', post_edit, name='post_edit'),
+    path('like/<pk>/', like_post, name='like_post'),
+    path('bookmark/<pk>/', bookmark_post, name='bookmark_post'),
+    path('comment/<pk>/', comment, name='comment'),
+    path('comment_delete/<pk>/', comment_delete, name='comment_delete'),
+    path('like_comment/<pk>/', like_comment, name='like_comment'),
 ]
