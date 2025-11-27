1add494d8a658f5d3b2abd97a8007710600f085b
Andreas Jud <andreas.jud@gmail.com>
2025-11-27 05:43:10 +0100

    Delete django/7 directory

--- django/7/a_network/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Follow
-
-@admin.register(Follow)
-class FollowAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 'follower', 'following')
--- django/7/a_network/templatetags/network_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from ..models import Follow
-
-register = template.Library()
-
-@register.filter
-def is_following(user, this_user):
-    is_following = Follow.objects.filter(follower=user, following=this_user).exists()
-    return is_following
